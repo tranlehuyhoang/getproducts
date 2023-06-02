6006f4d03f8c7a3ef3a06cea6c37dd1392d70f68
Huyh Huyhu <2509roblox@gmail.com>
2023-06-02 18:01:24 +0700

    new

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ import { Server } from "socket.io";
 import connect from "./database/connect.js";
 import router from "./routers/router.js";
  
-app.use(cors({
-
-  origin: ['https://tranlehuyhoang.github.io/mess_clientf', 'https://tranlehuyhoang.github.io', 'http://localhost:3000', "http://localhost:3000/mess_clientf"
-  ],
-  credentials: true
-
-}));
+app.use(cors());
 app.use(express.json());
 // connect();
 app.use('/api', router);
